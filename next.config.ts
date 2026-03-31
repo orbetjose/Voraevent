65fa6803e63fdbc35b31f0ebd378bcb9da152d90
Orbet Lozada <orbetjose@gmail.com>
2026-03-31 15:45:25 -0500

    Ajuste configuracion next para imagenes

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "admin.adempiregroup.com",
+        hostname: "admin.voraevents.com",
         port: "",
         pathname: "/**",
       },
